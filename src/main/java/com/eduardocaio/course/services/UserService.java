3e8416205c2b7fa825888cc1adcdfd30e4b984d8
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2024-07-05 18:11:01 -0300

    Service layer, component registration

---
+++ src/main/java/com/eduardocaio/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.eduardocaio.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eduardocaio.course.entities.User;
+import com.eduardocaio.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
