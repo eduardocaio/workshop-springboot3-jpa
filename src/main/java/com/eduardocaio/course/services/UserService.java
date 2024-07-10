62a4c3ff33bd3c231ad74c604730772ff2edd62a
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2024-07-10 12:12:30 -0300

    Dev profile

--- src/main/java/com/eduardocaio/course/services/UserService.java
+++ src/main/java/com/eduardocaio/course/services/UserService.java
@@ -4,12 +4,10 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.eduardocaio.course.entities.User;
 import com.eduardocaio.course.repositories.UserRepository;
-import com.eduardocaio.course.services.exceptions.DatabaseException;
 import com.eduardocaio.course.services.exceptions.ResourceNotFoundException;
 
 import jakarta.persistence.EntityNotFoundException;
